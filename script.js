3fc3283f68342ab95f8800426b9fc07e32e6e27e
Shiva953 <b22070@students.iitmandi.ac.in>
2023-02-09 12:40:28 +0530

    changed the overview

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let nm = document.getElementsByClassName("nme")[0];
-nm.addEventListener('mousemove',function(event) {nm.textContent='</Shiva>'});
+nm.addEventListener('mousemove',function(event) {nm.textContent='Shiva'});
 nm.addEventListener('mouseout',function(event) {nm.textContent='Shiva'});
 nm.textContent = 'Shiva';
 
@@ -42,6 +42,12 @@ for(let i=0;i<3;i+=1){
     titles[i].addEventListener('click',function(){window.open(link_projs[i],'_blank')})
 }
 
+const bt = document.getElementsByClassName('contact_button')[0]
+const socials = document.getElementById('socials')
+bt.addEventListener('click',()=>{
+  socials.scrollIntoView({behavior: 'smooth'});
+})
+
 const tps = Array.from(document.getElementsByClassName('flex_container')[0].childNodes).filter(node => node.nodeType!== Node.TEXT_NODE)
 const about = document.getElementsByClassName("main_about")[0]
 const skills = document.getElementsByClassName("skills")[0]
@@ -50,28 +56,41 @@ tps[0].addEventListener('click',function(){window.open('https://neutronsblog.has
 tps[1].addEventListener('click',function(){about.scrollIntoView({behavior: 'smooth'});});
 tps[2].addEventListener('click',function(){projects.scrollIntoView({behavior: 'smooth'});});
 tps[3].addEventListener('click',function(){skills.scrollIntoView({behavior: 'smooth'});});
-const btn = Array.from(document.getElementsByClassName('flex_icons')[0].childNodes).filter(node => node.nodeType!== Node.TEXT_NODE)
-const lnks = ["https://github.com/Shiva953","https://www.linkedin.com/in/shiva-seth-958114233/","https://www.reddit.com/user/Xiotus","https://discord.com/users/791137267535970324","https://twitter.com/Galactron71"]
-
 const fav = Array.from(document.getElementsByClassName('favourites')[0].childNodes).filter(node => node.nodeType!== Node.TEXT_NODE)
-fav[0].addEventListener('click',()=>{window.open('https://open.spotify.com/playlist/3vvXnLFktxlSLSHW0rSUog?si=7a64ff4e21674866&pt=c61a81d73c2c55592801c7eaa5645876','_blank')})
-fav[1].addEventListener('click',()=>{window.open('https://www.notion.so/All-Time-Favourite-Books-e82eef3e3058438190bbf1f3567b19a1','_blank')})
-fav[2].addEventListener('click',()=>{window.open('','_blank')})
-
-for (let i=0;i<=4;i+=1){
-  btn[i].addEventListener('click',()=>{window.open(lnks[i],'_blank')})
+    fav[0].addEventListener('click',()=>{window.open('https://open.spotify.com/playlist/3vvXnLFktxlSLSHW0rSUog?si=7a64ff4e21674866&pt=c61a81d73c2c55592801c7eaa5645876','_blank')})
+    fav[1].addEventListener('click',()=>{window.open('https://www.notion.so/All-Time-Favourite-Books-e82eef3e3058438190bbf1f3567b19a1','_blank')})
+    fav[2].addEventListener('click',()=>{window.open('','_blank')})
+const btn = Array.from(document.getElementsByClassName('flex_icons')[0].childNodes).filter(node => node.nodeType!== Node.TEXT_NODE)
+const btn2 = Array.from(document.getElementsByClassName('flex_icons')[1].childNodes).filter(node => node.nodeType!== Node.TEXT_NODE)
+const lnks = ["https://github.com/Shiva953","https://www.linkedin.com/in/shiva-seth-958114233/",'shivaset2@gmail.com',"https://discord.com/users/791137267535970324","https://twitter.com/Galactron71","https://www.reddit.com/user/Xiotus"]
+btn2[2].addEventListener('click',()=>{window.open(lnks[5],'_blank')})
+document.getElementById((`ic5`)).addEventListener("mousemove", function(event) {
+  const g = document.getElementById((`ic5`)).getElementsByTagName("g")[0];
+  g.setAttribute("fill", "#bdc4ff");
+});
+document.getElementById((`ic5`)).addEventListener("mouseout", function(event) {
+  const g = document.getElementById((`ic5`)).getElementsByTagName("g")[0];
+  g.setAttribute("fill", "#ffffff");
+});
+for (let i=0;i<=5;i+=1){
+  if(i<3){
+    btn[i].addEventListener('click',()=>{window.open(lnks[i],'_blank')})
+  }
+  else{
+    btn2[i-3].addEventListener('click',()=>{window.open(lnks[i],'_blank')})
+  }
   document.getElementById((`ic${i}`)).addEventListener("mousemove", function(event) {
       const g = document.getElementById((`ic${i}`)).getElementsByTagName("g")[0];
-      g.setAttribute("fill", "#e096ff");
-      btn[i].style.opacity = 1;
+      g.setAttribute("fill", "#bdc4ff");
     });
     document.getElementById((`ic${i}`)).addEventListener("mouseout", function(event) {
       const g = document.getElementById((`ic${i}`)).getElementsByTagName("g")[0];
       g.setAttribute("fill", "#ffffff");
-      btn[i].style.opacity = 0.8;
     });
-    tps[i].addEventListener('mousemove',()=>{tps[i].style.color='#97e5fc'})
-    tps[i].addEventListener('mouseout',()=>{tps[i].style.color='#ffffff'})
+    tps[i].addEventListener('mousemove',()=>{tps[i].style.color='#97e5fc';})
+    tps[i].addEventListener('mouseout',()=>{tps[i].style.color='#ffffff';})
     }
+
+
 
 
